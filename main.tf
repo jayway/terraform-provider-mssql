a47e8224ac1163fa5cc96e08f3a5cdb57d93bb81
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2019-01-24 16:25:54 +0100

    Demonstrate using a randomly generated password

--- main.tf
+++ main.tf
@@ -2,7 +2,21 @@ provider "mssql" {
   connection_string = "Data Source=localhost:1433;user id=sa;password=$up3R$3cR37"
 }
 
+provider "random" {}
+
+resource "random_string" "pwd" {
+  keepers = {
+    username = "terraform"
+  }
+
+  upper   = true
+  lower   = true
+  number  = true
+  special = true
+  length  = 23
+}
+
 resource "mssql_login" "test" {
-  username = "terraform"
-  password = "P@ssw0rd"
+  username = "${random_string.pwd.keepers.username}"
+  password = "${random_string.pwd.result}"
 }
